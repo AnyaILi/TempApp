0cb2462aa7f1503c91e9be6591e4471ef67bb1fe
Anya <anyali0219@gmail.com>
2023-08-19 14:21:53 -0700

    Initial Commit

---
+++ TempApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TempApp
+//
+//  Created by Anya Li on 8/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TempApp/TempAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempAppApp.swift
+//  TempApp
+//
+//  Created by Anya Li on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TempAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
